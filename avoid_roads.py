7eede3ebbd48c864cf21c35ce1ba7642043e5d8c
Nathx <nathankiner@gmail.com>
2016-09-22 12:56:13 +0200

    Avoid Roads - Dynamic Programming

--- avoid_roads.py
+++ avoid_roads.py
@@ -1,3 +1,5 @@
+# https://community.topcoder.com/stat?c=problem_statement&pm=1889&rd=4709
+
 # In the city, roads are arranged in a grid pattern. Each point on the grid represents a corner where two blocks meet. The points are connected by line segments which represent the various street blocks. Using the cartesian coordinate system, we can assign a pair of integers to each corner as shown below.
 #
 # You are standing at the corner with coordinates 0,0. Your destination is at corner width,height. You will return the number of distinct paths that lead to your destination. Each path must use exactly width+height blocks. In addition, the city has declared certain street blocks untraversable. These blocks may not be a part of any path. You will be given a String[] bad describing which blocks are bad. If (quotes for clarity) "a b c d" is an element of bad, it means the block from corner a,b to corner c,d is untraversable.
